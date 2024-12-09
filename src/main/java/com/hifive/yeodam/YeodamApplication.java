6fbe129cf84455dcf52a6bddec91502a709583c3
honginSon <sonhi2000@naver.com>
2024-12-09 18:04:17 +0900

    초기환경설정

---
+++ src/main/java/com/hifive/yeodam/YeodamApplication.java
@@ -0,0 +1,13 @@
+package com.hifive.yeodam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeodamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeodamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hifive/yeodam/YeodamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hifive.yeodam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeodamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
