988314e363b1a86e0c5135705bc678b5608c159e
jeongseockhyeon <jeong9260@naver.com>
2024-12-14 00:20:46 +0900

    Chore: 사용되지 않은 import 삭제

--- src/test/java/com/hifive/yeodam/ImageTest/serviceTest/ImageServiceTest.java
+++ src/test/java/com/hifive/yeodam/ImageTest/serviceTest/ImageServiceTest.java
@@ -1,8 +1,6 @@
 package com.hifive.yeodam.ImageTest.serviceTest;
 
 import com.hifive.yeodam.image.service.ImageService;
-import io.findify.s3mock.S3Mock;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
