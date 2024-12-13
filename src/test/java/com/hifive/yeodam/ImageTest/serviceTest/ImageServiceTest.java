98a9268c3360401d8953d7ff14b6faf77e106d5d
jeongseockhyeon <jeong9260@naver.com>
2024-12-14 00:20:00 +0900

    Feat: 테스트 코드 수정

--- src/test/java/com/hifive/yeodam/ImageTest/serviceTest/ImageServiceTest.java
+++ src/test/java/com/hifive/yeodam/ImageTest/serviceTest/ImageServiceTest.java
@@ -1,23 +1,18 @@
 package com.hifive.yeodam.ImageTest.serviceTest;
 
-import com.hifive.yeodam.ImageTest.config.S3MockConfig;
 import com.hifive.yeodam.image.service.ImageService;
 import io.findify.s3mock.S3Mock;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockMultipartFile;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
-@Import(S3MockConfig.class)
 @SpringBootTest
 public class ImageServiceTest {
-
-    @Autowired
-    private S3Mock s3Mock;
 
     @Autowired
     private ImageService imageService;
