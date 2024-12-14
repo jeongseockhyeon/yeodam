37f373f5080bded90fb5af0f36ea3d621a84423b
jeongseockhyeon <jeong9260@naver.com>
2024-12-14 23:13:55 +0900

    Feat: 여행 단일 조회 기능

--- src/main/java/com/hifive/yeodam/tour/repository/TourRepository.java
+++ src/main/java/com/hifive/yeodam/tour/repository/TourRepository.java
@@ -3,5 +3,5 @@ package com.hifive.yeodam.tour.repository;
 import com.hifive.yeodam.tour.entity.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TourRepository extends JpaRepository<Tour, Integer> {
+public interface TourRepository extends JpaRepository<Tour, Long> {
 }
