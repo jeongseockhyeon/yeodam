f5d2c541f2dacd7e68a46522ddd6a8667e68911f
jeongseockhyeon <jeong9260@naver.com>
2024-12-14 17:20:43 +0900

    Feat: 상품 전체 조회

--- src/main/java/com/hifive/yeodam/item/service/ItemService.java
+++ src/main/java/com/hifive/yeodam/item/service/ItemService.java
@@ -6,14 +6,19 @@ import com.hifive.yeodam.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ItemService {
+
+    /*상품 등록*/
     private final ItemRepository itemRepository;
     //private final SellerRepository sellerRepository
 
     public Item save(ItemReqDto itemReqDto) {
-        //Seller findSeller = sellerRepository.findById(itemReqDto.getSellerId()).orElseThrow(() -> new RuntimeException("해당 판매자가 없습니다."));
+        //Seller findSeller = sellerRepository.findById(itemReqDto.getSellerId())
+                                        // .orElseThrow(() -> new RuntimeException("해당 판매자가 없습니다."));
 
         Item item = Item.builder()
                 .sellerId(itemReqDto.getSellerId())
@@ -22,5 +27,10 @@ public class ItemService {
                 .build();
 
         return itemRepository.save(item);
+    }
+
+    /*상품 전체 조회*/
+    public List<Item> findAll() {
+        return itemRepository.findAll();
     }
 }
