3330ac3c05f9417d6d8f0fb1247c2b014741093d
jeongseockhyeon <jeong9260@naver.com>
2024-12-14 22:25:07 +0900

    Feat: 여행 등록 기능 테스트

---
+++ src/test/java/com/hifive/yeodam/tourTest/TourTest.java
@@ -0,0 +1,40 @@
+package com.hifive.yeodam.tourTest;
+
+import com.hifive.yeodam.tour.dto.TourReqDto;
+import com.hifive.yeodam.tour.service.TourService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.hifive.yeodam.tour.entity.Tour;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TourTest {
+
+    @Autowired
+    TourService tourService;
+
+    @Test
+    public void tourSaveTest(){
+        //given
+        String testRegion = "제주";
+        String period = "1일";
+        String des  = "test";
+        int price = 123123;
+
+        TourReqDto tourReqDto = new TourReqDto();
+        tourReqDto.setRegion(testRegion);
+        tourReqDto.setPeriod(period);
+        tourReqDto.setDescription(des);
+        tourReqDto.setPrice(price);
+
+        //when
+        Tour tour = tourService.save(tourReqDto);
+
+        assertEquals(testRegion,tour.getRegion());
+        assertEquals(period,tour.getPeriod());
+        assertEquals(des,tour.getDescription());
+        assertEquals(price,tour.getPrice());
+    }
+}
