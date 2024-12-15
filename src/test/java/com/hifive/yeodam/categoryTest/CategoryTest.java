20ee789f9a4105ce16d30bd2097ea4cbfcef5f8d
jeongseockhyeon <jeong9260@naver.com>
2024-12-16 01:36:32 +0900

    Feat: 카테고리 생성 api 테스트

---
+++ src/test/java/com/hifive/yeodam/categoryTest/CategoryTest.java
@@ -0,0 +1,68 @@
+package com.hifive.yeodam.categoryTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hifive.yeodam.category.dto.CategoryReqDto;
+import com.hifive.yeodam.category.entity.Category;
+import com.hifive.yeodam.category.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CategoryTest {
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @BeforeEach
+    public void setMockMvc(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    @DisplayName("카테고리 생성 테스트")
+    public void saveCategoryTest() throws Exception {
+
+        //given
+        String categoryName = "액티비티";
+        CategoryReqDto categoryReqDto = new CategoryReqDto();
+        categoryReqDto.setCategoryName(categoryName);
+        String url = "/api/category";
+
+        String json = objectMapper.writeValueAsString(categoryReqDto);
+
+        //when
+        ResultActions result = mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON).content(json));
+        List<Category> categories = categoryService.findAllCategory();
+        Category category = categories.get(0);
+
+        //then
+        result.andExpect(status().isCreated());
+        assertEquals(categoryName, category.getName());
+    }
+
+
+
+}
