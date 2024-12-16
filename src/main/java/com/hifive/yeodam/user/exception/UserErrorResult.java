78ac0a6376c86cd4fbceeee7885d18b89b69c24c
honginSon <sonhi2000@naver.com>
2024-12-16 21:12:24 +0900

    Feat: 유저 닉네임 중복검사 로직

--- src/main/java/com/hifive/yeodam/user/exception/UserErrorResult.java
+++ src/main/java/com/hifive/yeodam/user/exception/UserErrorResult.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorResult {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+    DUPLICATED_NICKNAME_JOIN(HttpStatus.BAD_REQUEST, "Duplicated Nickname Join Request"),
     UNKNOWN_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown Exception"),
     ;
 
