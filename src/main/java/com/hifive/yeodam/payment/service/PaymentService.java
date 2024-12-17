54d1a71994b12714c8981152f0bb25579915b65a
KeeYong <ki425125@gmail.com>
2024-12-17 23:35:24 +0900

    feat: 결제 검증로직 개발

--- src/main/java/com/hifive/yeodam/payment/service/PaymentService.java
+++ src/main/java/com/hifive/yeodam/payment/service/PaymentService.java
@@ -3,7 +3,6 @@ package com.hifive.yeodam.payment.service;
 
 import com.hifive.yeodam.order.domain.Order;
 import com.hifive.yeodam.order.repository.OrderRepository;
-import com.hifive.yeodam.orderdetail.domain.OrderDetail;
 import com.hifive.yeodam.payment.domain.Payment;
 import com.hifive.yeodam.payment.dto.PaymentRequest;
 import com.hifive.yeodam.payment.dto.PaymentRequestCallBack;
@@ -12,13 +11,11 @@ import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-import static com.hifive.yeodam.payment.domain.PaymentStatus.COMPLETED;
-
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -29,10 +26,11 @@ public class PaymentService {
     private final IamportClient iamportClient;
 
     public Long createPayment(String orderUid) {
+
         Order order = orderRepository.findByOrderUid(orderUid)
                 .orElseThrow(() -> new IllegalArgumentException("일치하는 주문이 없습니다"));
 
-        int totalPrice = getTotalPrice(order);
+        int totalPrice = order.getTotalPrice();
         Payment payment = Payment.create(totalPrice, order);
 
         paymentRepository.save(payment);
@@ -46,75 +44,75 @@ public class PaymentService {
                 .orElseThrow(() -> new IllegalArgumentException("일치하는 결제정보가 없습니다"));
 
         return PaymentRequest.builder()
-                .itemName(getItemName(payment.getOrder()))
+                .itemName(payment.getOrder().getItemName())
                 .price(payment.getPrice())
                 .orderUid(payment.getOrder().getOrderUid())
                 .build();
     }
 
-    //TODO
-    public void paymentCallBack(PaymentRequestCallBack request) {
+    //응답받은 결제 검증 로직
+    public IamportResponse<com.siot.IamportRestClient.response.Payment> paymentCallBack
+    (PaymentRequestCallBack request) {
 
         //결제 단건 조회
         IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse = iamportClient
                 .paymentByImpUid(request.getPaymentUid());
 
+
+        //ORDER, Payment 조회
         Order order = orderRepository.findByOrderUid(request.getOrderUid())
                 .orElseThrow(() -> new IllegalArgumentException("일치하는 주문이 없습니다"));
 
-        Payment payment = paymentRepository.findByOrderId(order.getId());
+        Payment payment = order.getPayment();
 
-        //결제가 안된 경우 데이터 삭제
-        if (!iamportResponse.getResponse().getStatus().equals("paid")) {
-            paymentRepository.deleteById(payment.getId());
-            orderRepository.deleteById(order.getId());
+        //결제가 성공 검증
+        validateIsSuccess(iamportResponse, payment, order);
 
-            throw new RuntimeException("결제 미완료");
-        }
-
-        int orderTotalPrice = order.getOrderDetails().stream()
-                .mapToInt(OrderDetail::getTotalPrice)
-                .sum();
-
+        int orderTotalPrice = order.getTotalPrice();
         int iamportPrice = iamportResponse.getResponse().getAmount().intValue();
 
         //결제 위변조 체크
+        validatePaymentPrice(orderTotalPrice, iamportPrice, payment, order, iamportResponse);
+
+        payment.successPayment(iamportResponse.getResponse().getImpUid());
+        order.successOrder();
+
+        return iamportResponse;
+    }
+
+    public void paymentFail(PaymentRequestCallBack request) {
+
+        Order order = orderRepository.findByOrderUid(request.getOrderUid())
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 결제가 없습니다"));
+
+        order.failOrder();
+
+        log.info(request.toString());
+        order.getPayment().failPayment(request.getPaymentUid());
+    }
+
+    private void validateIsSuccess(IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse, Payment payment, Order order) {
+        if (!iamportResponse.getResponse().getStatus().equals("paid")) {
+            deletePaymentOrder(payment, order);
+            throw new RuntimeException("결제 미완료");
+        }
+    }
+
+    public void deletePaymentOrder(Payment payment, Order order) {
+        paymentRepository.deleteById(payment.getId());
+        orderRepository.deleteById(order.getId());
+    }
+
+    private void validatePaymentPrice(int orderTotalPrice, int iamportPrice, Payment payment, Order order,
+                                      IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse) {
+
         if (orderTotalPrice != iamportPrice) {
-            paymentRepository.deleteById(payment.getId());
-            orderRepository.deleteById(order.getId());
+            deletePaymentOrder(payment, order);
 
             iamportClient.cancelPaymentByImpUid(
                     new CancelData(iamportResponse.getResponse().getImpUid(), true));
 
-            throw new RuntimeException("결제 위변조");
+            throw new RuntimeException("결제 위변조가 의심됩니다.");
         }
-
-        payment.changePaymentBySuccess(COMPLETED, iamportResponse.getResponse().getImpUid());
-        order.successOrder();
-    }
-
-    private int getTotalPrice(Order order) {
-        return order.getOrderDetails().stream()
-                .mapToInt(OrderDetail::getTotalPrice)
-                .sum();
-    }
-
-    private String getItemName(Order order) {
-        StringBuilder sb = new StringBuilder();
-        List<OrderDetail> orderDetails = order.getOrderDetails();
-
-        int count = orderDetails.size();
-        if (count > 1) {
-
-            sb.append(orderDetails.getFirst().getItem())
-                    .append(" 외 ")
-                    .append(count)
-                    .append("건");
-
-            return sb.toString();
-        }
-
-        return sb.append(orderDetails.getFirst().getItem())
-                .toString();
     }
 }
