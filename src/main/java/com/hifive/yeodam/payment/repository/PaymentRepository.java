57a50c71182c2727ea4ef395b1997247e3cfa91f
KeeYong <ki425125@gmail.com>
2024-12-17 23:47:35 +0900

    refactor: 필요없는 import 제거

--- src/main/java/com/hifive/yeodam/payment/repository/PaymentRepository.java
+++ src/main/java/com/hifive/yeodam/payment/repository/PaymentRepository.java
@@ -2,9 +2,7 @@ package com.hifive.yeodam.payment.repository;
 
 import com.hifive.yeodam.payment.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
