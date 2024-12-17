071e5f06e9b0ce32b1335fd53d0fb734aa668143
KeeYong <ki425125@gmail.com>
2024-12-17 23:39:21 +0900

    feat: 요청시 받는 필드 추가

--- src/main/java/com/hifive/yeodam/order/dto/AddOrderRequest.java
+++ src/main/java/com/hifive/yeodam/order/dto/AddOrderRequest.java
@@ -1,17 +1,30 @@
 package com.hifive.yeodam.order.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddOrderRequest {
-    private Long userId;
-    private List<ItemRequest> items;
+
+    private String bookerName;
+    private String phoneNumber;
+    private String orderMessage;
+    private List<ItemRequest> items = new ArrayList<>();
 
     @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class ItemRequest {
-        private Long itemId;
+        private Long id;
+        private String name;
         private int count;
+        private String period;
+        private int price;
     }
 }
