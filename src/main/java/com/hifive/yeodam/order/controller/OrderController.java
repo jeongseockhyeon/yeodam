754b1a555de6b2c3efe48e6f03f70cbdc887dd37
KeeYong <ki425125@gmail.com>
2024-12-17 23:32:54 +0900

    feat: 테스트용 principal 객체 추가 추후 시큐리티 연동후 변경 예정

--- src/main/java/com/hifive/yeodam/order/controller/OrderController.java
+++ src/main/java/com/hifive/yeodam/order/controller/OrderController.java
@@ -3,12 +3,17 @@ package com.hifive.yeodam.order.controller;
 import com.hifive.yeodam.order.dto.AddOrderRequest;
 import com.hifive.yeodam.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
@@ -16,15 +21,41 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping("/order")
-    public String orderForm() {
-        return "order/orderForm";
+    public String testOrderForm(Model model) {
+
+        AddOrderRequest.ItemRequest itemRequest = new AddOrderRequest.ItemRequest();
+        itemRequest.setId(1L);
+        itemRequest.setName("제주도 푸른밤");
+        itemRequest.setPrice(100);
+        itemRequest.setCount(2);
+        List<AddOrderRequest.ItemRequest> items = List.of(itemRequest);
+
+        model.addAttribute("orderRequest",
+                new AddOrderRequest("길동이", "1234-1234", null, items));
+
+        return "order/order-form";
     }
 
     @PostMapping("/order")
-    public String order(AddOrderRequest request, RedirectAttributes redirectAttributes) {
+    public String order(/*Principal principal,*/ AddOrderRequest request, RedirectAttributes redirectAttributes) {
 
-        redirectAttributes.addAttribute("orderUid", orderService.order(request));
+        Principal principal = new TestPrincipal("123@a.com");
+
+        redirectAttributes.addAttribute("orderUid", orderService.order(request, principal));
         return "redirect:/payment?orderUid={orderUid}";
+    }
 
+    static class TestPrincipal implements Principal {
+
+        private String name;
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+
+        public TestPrincipal(String name) {
+            this.name = name;
+        }
     }
 }
