70aae3cc52fc8dad74c74da6c13e9779ffd33d48
Ellie <2unmi25@naver.com>
2024-12-18 16:42:44 +0900

    feat: 판매자 로그인 요청 dto

--- src/main/java/com/hifive/yeodam/seller/repository/SellerRepository.java
+++ src/main/java/com/hifive/yeodam/seller/repository/SellerRepository.java
@@ -3,6 +3,9 @@ package com.hifive.yeodam.seller.repository;
 import com.hifive.yeodam.seller.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+    Optional<Seller> findByAuthId(Long authId);
 }
 
