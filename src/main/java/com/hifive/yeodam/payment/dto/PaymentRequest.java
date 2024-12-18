7b1c86f403d60da7981c8cded7b2e99e58bba73b
KeeYong <ki425125@gmail.com>
2024-12-18 10:17:26 +0900

    feat: 필드 추가

--- src/main/java/com/hifive/yeodam/payment/dto/PaymentRequest.java
+++ src/main/java/com/hifive/yeodam/payment/dto/PaymentRequest.java
@@ -6,12 +6,18 @@ import lombok.Data;
 @Data
 public class PaymentRequest {
     private String orderUid;
+    private String username;
+    private String phone;
+    private String email;
     private String itemName;
     private int price;
 
     @Builder
-    public PaymentRequest(String orderUid, String itemName, int price) {
+    public PaymentRequest(String orderUid, String username,String phone, String email, String itemName, int price) {
         this.orderUid = orderUid;
+        this.username = username;
+        this.phone = phone;
+        this.email = email;
         this.itemName = itemName;
         this.price = price;
     }
