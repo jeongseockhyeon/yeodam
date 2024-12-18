f366c3eddbba5d0dbba1890f22627738c4bc1c9e
KeeYong <ki425125@gmail.com>
2024-12-18 16:07:36 +0900

    feat: 테스트용 mock 삭제

--- src/test/java/com/hifive/yeodam/order/service/code/MockUserRespiratory.java
+++
@@ -1,9 +0,0 @@
-package com.hifive.yeodam.order.service.code;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MockUserRespiratory extends JpaRepository<MockUser, Long> {
-    Optional<MockUser> findById(Long id);
-}
