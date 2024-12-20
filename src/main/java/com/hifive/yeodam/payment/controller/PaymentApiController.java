9f40522532417878797212972c006dc75fa1e3f0
KeeYong <ki425125@gmail.com>
2024-12-21 01:02:04 +0900

    feat: 결제 전 브라우저 종류 시 수정 로직 추가

--- src/main/java/com/hifive/yeodam/payment/controller/PaymentApiController.java
+++ src/main/java/com/hifive/yeodam/payment/controller/PaymentApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+import static com.hifive.yeodam.global.constant.PaymentConst.ORDER_UID;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/payment")
@@ -19,21 +21,32 @@ public class PaymentApiController {
     private final PaymentService paymentService;
 
     @PostMapping
-    public ResponseEntity<PaymentResponse> paymentForm(@RequestParam("orderUid") String orderUid) {
+    public ResponseEntity<PaymentResponse> createPayment(@RequestParam(ORDER_UID) String orderUid) {
         Long paymentId = paymentService.createPayment(orderUid);
         PaymentResponse paymentResponse = paymentService.findRequestPayment(paymentId); //커멘드 쿼리 분리
         return ResponseEntity.ok(paymentResponse);
     }
 
-    @PostMapping("/success")
+    @PostMapping("/fail")
+    public ResponseEntity failPayment(@RequestBody PaymentRequestCallBack request) {
+        paymentService.paymentFail(request);
+        return ResponseEntity.ok(Map.of(ORDER_UID, request.getOrderUid()));
+    }
+
+    @PostMapping("/check")
+    public void checkPaymentStatus(@RequestParam(name = ORDER_UID) String orderUid) {
+        paymentService.checkPaymentStatus(orderUid);
+    }
+
+    @PostMapping("/validate")
     public ResponseEntity validationPayment(@RequestBody PaymentRequestCallBack request) {
         String orderUid = paymentService.validatePayment(request);
         return ResponseEntity.ok(Map.of("orderUid", orderUid));
     }
 
     @PostMapping("/cancel")
-    public String cancelPayment(@RequestParam("orderUid") String orderUid) {
+    public ResponseEntity cancelPayment(@RequestParam(ORDER_UID) String orderUid) {
         paymentService.cancel(orderUid);
-        return "ok";
+        return ResponseEntity.ok(Map.of(ORDER_UID, orderUid));
     }
 }
