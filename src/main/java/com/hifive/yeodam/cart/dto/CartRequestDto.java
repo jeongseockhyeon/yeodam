d4e8cf1e6abc4521771fdddf2f4ce029c4be6765
yookyung <cheshirejyoo@gmail.com>
2024-12-20 10:52:43 +0900

    예약/일반 상품 구분 추가

--- src/main/java/com/hifive/yeodam/cart/dto/CartRequestDto.java
+++ src/main/java/com/hifive/yeodam/cart/dto/CartRequestDto.java
@@ -8,9 +8,11 @@ import lombok.Setter;
 public class CartRequestDto {
     private Long itemId;
     private int count; //추가할 상품 수량
+    private boolean reservation;
 
-    public CartRequestDto(Long itemId, int count) {
+    public CartRequestDto(Long itemId, int count, boolean reservation) {
         this.itemId = itemId;
         this.count = count;
+        this.reservation = reservation;
     }
 }
