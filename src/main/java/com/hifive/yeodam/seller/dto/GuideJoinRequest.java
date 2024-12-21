e725bce3c655dbb2c39a62b01d5a75f418f74c8d
Ellie <2unmi25@naver.com>
2024-12-21 20:54:03 +0900

    fix: 판매자, 가이드 관련 dto 수정

--- src/main/java/com/hifive/yeodam/seller/dto/GuideJoinRequest.java
+++ src/main/java/com/hifive/yeodam/seller/dto/GuideJoinRequest.java
@@ -3,6 +3,7 @@ package com.hifive.yeodam.seller.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,8 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class GuideJoinRequest {
     @NotBlank
+    @Size(max = 25)
+    @Pattern(regexp = "^[a-zA-Z가-힣]+$", message = "이름은 영어 또는 한글로만 입력 가능합니다.")
     private String name;
 
     @Past
@@ -25,7 +28,7 @@ public class GuideJoinRequest {
     private String gender;
 
     @NotBlank
-    @Pattern(regexp = "^010\\d{8}$")
+    @Pattern(regexp = "^010\\d{8}$", message = "전화번호는 010으로 시작하는 11자리 숫자만 가능합니다.")
     private String phone;
 
     @NotBlank
