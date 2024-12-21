9c4b91a4e79563b3adc64828ee1266a986c3e95d
jeongseockhyeon <jeong9260@naver.com>
2024-12-22 04:57:29 +0900

    Chore: 이미지 업로드 테스트 수정

--- src/test/java/com/hifive/yeodam/ImageTest/serviceTest/ImageServiceTest.java
+++ src/test/java/com/hifive/yeodam/ImageTest/serviceTest/ImageServiceTest.java
@@ -1,19 +1,38 @@
 package com.hifive.yeodam.ImageTest.serviceTest;
 
 import com.hifive.yeodam.image.service.ImageService;
+import io.findify.s3mock.S3Mock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.annotation.Import;
 import org.springframework.mock.web.MockMultipartFile;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 
-@SpringBootTest
+@Import(MockS3Config.class)
 public class ImageServiceTest {
 
-    @Autowired
+    @Mock
     private ImageService imageService;
+
+    @Mock
+    private S3Mock s3Mock;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        s3Mock.stop();
+    }
 
     @Test
     public void uploadTest(){
@@ -24,12 +43,15 @@ public class ImageServiceTest {
 
         MockMultipartFile mockMultipartFile = new MockMultipartFile(onlyName,imageName, contentType,onlyName.getBytes());
 
+        when(imageService.upload(mockMultipartFile)).thenReturn("http://localhost:8001/test.png"); //임시 이미지 url
+
         //when
         String url = imageService.upload(mockMultipartFile);
 
         //then
         assertThat(url).contains(onlyName);
-    }
+        verify(imageService,times(1)).upload(mockMultipartFile);
 
+    }
 
 }
