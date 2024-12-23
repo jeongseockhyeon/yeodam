7c9719fdb68120782324c71e67e5a30f0a393d49
honginSon <sonhi2000@naver.com>
2024-12-23 18:11:07 +0900

    feat: 회원 & 인증 테스트코드 추가

--- src/test/java/com/hifive/yeodam/auth/service/AuthServiceTest.java
+++ src/test/java/com/hifive/yeodam/auth/service/AuthServiceTest.java
@@ -8,6 +8,7 @@ import com.hifive.yeodam.auth.repository.RoleRepository;
 import com.hifive.yeodam.user.dto.JoinRequest;
 import com.hifive.yeodam.auth.entity.Auth;
 import com.hifive.yeodam.auth.repository.AuthRepository;
+import com.hifive.yeodam.user.dto.UserUpdateRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,14 +17,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
@@ -39,6 +42,9 @@ public class AuthServiceTest {
 
     @Mock
     private RoleRepository roleRepository;
+
+    @Mock
+    private BindingResult errorResult;
 
     private static final String email = "emailName@domain.com";
     private static final String password = "1234";
@@ -92,6 +98,80 @@ public class AuthServiceTest {
 
         //then
         assertThat(result).isTrue();
+    }
+
+    @Test
+    public void 이메일중복시에러값추가() throws Exception{
+        //given
+        JoinRequest request = JoinRequest.builder().email(email).build();
+
+        doReturn(true).when(authRepository).existsByEmail(email);
+
+        //when
+        target.checkDuplicatedEmail(request, errorResult);
+
+        //then
+
+        //verify
+        verify(errorResult, times(1)).addError(any(FieldError.class));
+    }
+
+    @Test
+    public void 인증조회실패_인증정보존재하지않음() throws Exception{
+        //given
+        doReturn(Optional.empty()).when(authRepository).findById(-1L);
+
+        //when
+        AuthException result = assertThrows(AuthException.class, () -> target.getAuth(-1L));
+
+        //then
+        assertThat(result.getErrorResult()).isEqualTo(AuthErrorResult.AUTH_NOT_FOUND);
+    }
+
+    @Test
+    public void 인증조회성공() throws Exception{
+        //given
+        doReturn(Optional.of(Auth.builder()
+                .email(email)
+                .build())).when(authRepository).findById(-1L);
+
+        //when
+        Auth result = target.getAuth(-1L);
+
+        //then
+        assertThat(result.getEmail()).isEqualTo(email);
+    }
+
+    @Test
+    public void 인증수정실패_인증정보존재하지않음() throws Exception{
+        //given
+        UserUpdateRequest request = UserUpdateRequest.builder().build();
+        doReturn(Optional.empty()).when(authRepository).findById(-1L);
+
+        //when
+        AuthException result = assertThrows(AuthException.class, () -> target.updateAuth(-1L, request));
+
+        //then
+        assertThat(result.getErrorResult()).isEqualTo(AuthErrorResult.AUTH_NOT_FOUND);
+    }
+
+    @Test
+    public void 인증수정성공() throws Exception{
+        //given
+        UserUpdateRequest request = UserUpdateRequest.builder()
+                .password("updatedPwd")
+                .build();
+
+        doReturn(Optional.of(Auth.builder().build()))
+                .when(authRepository).findById(-1L);
+
+        doReturn("encryptedPwd").when(passwordEncoder).encode("updatedPwd");
+
+        //when
+        Auth result = target.updateAuth(-1L, request);
+
+        //then
+        assertThat(result.getPassword()).isNotNull();
     }
 
     private Auth auth() {
