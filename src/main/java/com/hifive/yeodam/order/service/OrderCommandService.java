2706a87990bee696a554825e56ca149c2b574a0e
KeeYong <ki425125@gmail.com>
2024-12-26 22:43:59 +0900

    Refactor: command, query로 분리

--- src/main/java/com/hifive/yeodam/order/service/OrderCommandService.java
+++ src/main/java/com/hifive/yeodam/order/service/OrderCommandService.java
@@ -7,13 +7,14 @@ import com.hifive.yeodam.order.domain.Order;
 import com.hifive.yeodam.order.dto.request.AddOrderRequest;
 import com.hifive.yeodam.order.dto.request.CancelOrderRequest;
 import com.hifive.yeodam.order.dto.response.CancelOrderResponse;
+import com.hifive.yeodam.order.dto.response.CreateOrderResponse;
 import com.hifive.yeodam.order.repository.OrderRepository;
 import com.hifive.yeodam.orderdetail.domain.OrderDetail;
 import com.hifive.yeodam.orderdetail.domain.OrderDetailsStatus;
 import com.hifive.yeodam.user.entity.User;
+import com.hifive.yeodam.user.exception.UserException;
 import com.hifive.yeodam.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -23,10 +24,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.hifive.yeodam.global.exception.CustomErrorCode.*;
-import static com.hifive.yeodam.global.exception.CustomErrorCode.NOT_ENOUGH_STOCK;
 import static com.hifive.yeodam.order.domain.OrderStatus.CANCELED;
 import static com.hifive.yeodam.order.domain.OrderStatus.FAILED;
 import static com.hifive.yeodam.orderdetail.domain.OrderDetailsStatus.USED;
+import static com.hifive.yeodam.user.exception.UserErrorResult.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -37,16 +38,21 @@ public class OrderCommandService {
     private final UserRepository userRepository;
 
     @Transactional
-    public String order(AddOrderRequest request, Principal principal) {
+    public CreateOrderResponse order(AddOrderRequest request, Principal principal) {
 
         User user = getUserByEmail(principal);
         List<OrderDetail> orderDetails = createOrderDetails(request);
-        int totalPrice = orderDetails.stream().mapToInt(OrderDetail::getTotalPrice).sum();
-        Order order = Order.createOrder(user, totalPrice, orderDetails);
 
+        int totalPrice = orderDetails.stream()
+                .mapToInt(OrderDetail::getTotalPrice)
+                .sum();
+
+        Order order = Order.createOrder(user, totalPrice, orderDetails);
         orderRepository.save(order);
 
-        return order.getOrderUid();
+        return CreateOrderResponse.builder()
+                .orderUid(order.getOrderUid())
+                .build();
     }
 
     @Transactional
@@ -57,38 +63,25 @@ public class OrderCommandService {
 
         List<OrderDetail> orderDetails = order.getOrderDetails();
 
-        if (List.of(FAILED, CANCELED).contains(order.getStatus())) {
-            throw new CustomException(ORDER_CAN_NOT_CANCEL);
-        }
-
-        orderDetails.stream()
-                .filter(od -> od.getStatus().equals(USED))
-                .forEach(od -> {
-                    throw new CustomException(ORDER_CAN_NOT_CANCEL);
-                });
+        validateOrderStatus(order);
+        validateDetailStatus(orderDetails);
 
         orderDetails.forEach(od -> od.changeStatus(OrderDetailsStatus.CANCELED));
         order.chanceOrderStatus(CANCELED);
 
-        int cancelPrice = orderDetails.stream()
-                .mapToInt(OrderDetail::getTotalPrice)
-                .sum();
-
-        return CancelOrderResponse.builder()
-                .orderUid(order.getOrderUid())
-                .totalPrice(cancelPrice)
-                .build();
+        return getCancelOrderResponse(order, getCancelPrice(orderDetails));
     }
 
     private User getUserByEmail(Principal principal) {
         return userRepository.findByEmail(principal.getName())
-                .orElseThrow(() -> new IllegalArgumentException("일치하는_회원이_없습니다"));
+                .orElseThrow(() -> new UserException(USER_NOT_FOUND));
     }
 
     private List<OrderDetail> createOrderDetails(AddOrderRequest orderRequests) {
 
         List<OrderDetail> orderDetails = new ArrayList<>();
-        for (AddOrderRequest.orderRequest request : orderRequests.getOrderRequests()) {
+
+        orderRequests.getOrderRequests().forEach(request -> {
 
             Item item = itemRepository.findById(request.getItemId())
                     .orElseThrow(() -> new CustomException(ITEM_NOT_FOUND));
@@ -99,13 +92,8 @@ public class OrderCommandService {
 
             item.removeStock();
             validateOrderMessage(request);
-
-            OrderDetail orderDetail = OrderDetail.create(
-                    item, request.getCount(), item.getPrice(),
-                    request.getBookerName(), request.getPhoneNumber(), request.getOrderMessage());
-
-            orderDetails.add(orderDetail);
-        }
+            orderDetails.add(createOrderDetail(request, item));
+        });
 
         return orderDetails;
     }
@@ -113,5 +101,39 @@ public class OrderCommandService {
     private void validateOrderMessage(AddOrderRequest.orderRequest request) {
         if (!StringUtils.hasText(request.getOrderMessage()))
             request.setOrderMessage("메세지 없음");
+    }
+
+    private void validateDetailStatus(List<OrderDetail> orderDetails) {
+        orderDetails.stream()
+                .filter(od -> od.getStatus().equals(USED))
+                .forEach(od -> {
+                    throw new CustomException(ORDER_CAN_NOT_CANCEL);
+                });
+    }
+
+    private void validateOrderStatus(Order order) {
+        if (List.of(FAILED, CANCELED).contains(order.getStatus())) {
+            throw new CustomException(ORDER_CAN_NOT_CANCEL);
+        }
+    }
+
+    private int getCancelPrice(List<OrderDetail> orderDetails) {
+        return orderDetails.stream()
+                .mapToInt(OrderDetail::getTotalPrice)
+                .sum();
+    }
+
+    private CancelOrderResponse getCancelOrderResponse(Order order, int cancelPrice) {
+        return CancelOrderResponse.builder()
+                .orderUid(order.getOrderUid())
+                .totalPrice(cancelPrice)
+                .build();
+    }
+
+    private OrderDetail createOrderDetail(AddOrderRequest.orderRequest request, Item item) {
+        return OrderDetail.create(
+                item, request.getCount(), item.getPrice(),
+                request.getBookerName(), request.getPhoneNumber(), request.getOrderMessage()
+        );
     }
 }
