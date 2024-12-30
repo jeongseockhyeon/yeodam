d942b0a415c989b7aa36494b4d674b6d8bbc2c3a
jeongseokhyeon <jeong9235@gmail.com>
2024-12-30 13:40:43 +0900

    Chore: 상품 사진 저장 삭제 로직 수정

---
+++ src/main/java/com/hifive/yeodam/item/service/ItemImageService.java
@@ -0,0 +1,36 @@
+package com.hifive.yeodam.item.service;
+
+import com.hifive.yeodam.image.service.ImageService;
+import com.hifive.yeodam.item.entity.Item;
+import com.hifive.yeodam.item.entity.ItemImage;
+import com.hifive.yeodam.item.repository.ItemImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@Service
+public class ItemImageService {
+    private final ItemImageRepository itemImageRepository;
+    private final ImageService imageService;
+
+    @Transactional
+    public void save(MultipartFile imageFile, Item targetItem) {
+        String originalName = imageFile.getOriginalFilename();
+        String storePath = imageService.upload(imageFile);
+
+        ItemImage itemImage = ItemImage.builder()
+                .item(targetItem)
+                .originalName(originalName)
+                .storePath(storePath)
+                .build();
+
+        itemImageRepository.save(itemImage);
+    }
+
+    @Transactional
+    public void delete(Long imageId) {
+        itemImageRepository.deleteById(imageId);
+    }
+}
