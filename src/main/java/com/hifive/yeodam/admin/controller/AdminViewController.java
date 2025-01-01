b2166964673236ec3bb39486f7f50dd885ef3bf3
jeongseokhyeon <jeong9235@gmail.com>
2025-01-02 00:49:13 +0900

    Feat: 관리자 수정 페이지 구현

--- src/main/java/com/hifive/yeodam/admin/controller/AdminViewController.java
+++ src/main/java/com/hifive/yeodam/admin/controller/AdminViewController.java
@@ -2,6 +2,7 @@ package com.hifive.yeodam.admin.controller;
 
 import com.hifive.yeodam.auth.entity.Auth;
 import com.hifive.yeodam.user.dto.UserResponse;
+import com.hifive.yeodam.user.dto.UserUpdateRequest;
 import com.hifive.yeodam.user.service.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,26 @@ public class AdminViewController {
         UserResponse userResponse = userService.getUserByAuth(auth);
         model.addAttribute("admin", userResponse);
         return "admin/manage-page";
+    }
+
+    @GetMapping("/edit")
+    public String userEditView(@AuthenticationPrincipal Auth auth, Model model) {
+
+        UserResponse userResponse = userService.getUserByAuth(auth);
+
+        UserUpdateRequest userUpdateRequest = UserUpdateRequest.builder()
+                .email(auth.getEmail())
+                .password(auth.getPassword())
+                .name(userResponse.getName())
+                .nickname(userResponse.getNickname())
+                .phone(userResponse.getPhone())
+                .birthDate(userResponse.getBirthDate())
+                .gender(userResponse.getGender())
+                .build();
+
+        model.addAttribute("userUpdateRequest", userUpdateRequest);
+
+        return "admin/admin-edit";
     }
 
 }
