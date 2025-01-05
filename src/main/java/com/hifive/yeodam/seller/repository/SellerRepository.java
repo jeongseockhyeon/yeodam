66824db1bd48d61d8f7148b05f4145512e0fa6f5
KeeYong <ki425125@gmail.com>
2025-01-05 18:12:29 +0900

    feat: user,seller 리뷰 조회 추가

--- src/main/java/com/hifive/yeodam/seller/repository/SellerRepository.java
+++ src/main/java/com/hifive/yeodam/seller/repository/SellerRepository.java
@@ -2,10 +2,14 @@ package com.hifive.yeodam.seller.repository;
 
 import com.hifive.yeodam.seller.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
     Optional<Seller> findByAuthId(Long authId);
+
+    @Query("select s from Seller s join s.auth a where a.email = :email")
+    Optional<Seller> findByEmail(String email);
 }
 
